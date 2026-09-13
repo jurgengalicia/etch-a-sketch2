f8194ed22f79b66813208584852de1bcf107f79a
JurgenGalicia <jurgen.galicia@gmail.com>
2026-09-13 00:16:04 -0700

    etch board can now be build a 2d grid of divs

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let boardContainer = document.querySelector(".board-container");
+let boardWidth = 500;
+let boardHeight = 500;
+boardContainer.style.width = `${boardWidth}px`;
+boardContainer.style.height = `${boardHeight}px`;
 
 
-function buildBoard(length, width){
+
+function buildBoard(height, width){
   while(boardContainer.firstChild){
     boardContainer.removeChild(boardContainer.firstChild);
   }
-  for(let x=0; x <= length; x++){
-    for(let y=0; y <= width; y++){
+  for(let x=0; x < height; x++){
+    for(let y=0; y < width; y++){
       let currTile = document.createElement("div");
-      
+      currTile.style.width = `${boardWidth/width}px`;
+      currTile.style.height = `${boardHeight/height}px`;
       boardContainer.appendChild(currTile);
     }
   }
